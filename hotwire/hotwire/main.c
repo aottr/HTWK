69aed5e59ec988959721b8dde0f2f839a3b38a8a
Dustin Kröger <me@staubrein.com>
2019-10-24 23:37:33 +0200

    init
    Create project for ATTiny261

---
+++ hotwire/hotwire/main.c
@@ -0,0 +1,18 @@
+/*
+ * hotwire.c
+ *
+ * Created: 24.10.2019 23:33:20
+ * Author : Dustin Kröger, Max Matkowitz
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+#include <avr/interrupt.h>
+
+int main(void) {
+	
+    while (1) {
+		
+    }
+}
+
