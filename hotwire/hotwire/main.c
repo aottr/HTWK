0c5d2a67c1b43bb19fd2da30f5c2c6220856606c
Dustin Kröger <me@staubrein.com>
2019-10-25 08:48:27 +0200

    Blinking

--- hotwire/hotwire/main.c
+++ hotwire/hotwire/main.c
@@ -1,18 +1,33 @@
 /*
  * hotwire.c
  *
+ * Microcontroller: ATTiny261A
+ *
  * Created: 24.10.2019 23:33:20
  * Author : Dustin Kröger, Max Matkowitz
+ * Version: 0.1 Blinking
  */ 
 
+#define F_CPU 8000000
 #include <avr/io.h>
 #include <avr/delay.h>
 #include <avr/interrupt.h>
 
 int main(void) {
+
+	DDRA |= 0b11111000;
+	PORTB &= ~(1 << PB3); // PB3 ground
+
+	PORTA=0;
+	PORTB=0;
 	
     while (1) {
-		
+
+    	//PORTA |= (1 << PA3); // PA3 goes high
+    	_delay_ms(500);
+    	//PORTA &= ~(1 << PA3); // PA3 goes low
+
+    	PORTA ^= 0b11111000;
     }
 }
 
