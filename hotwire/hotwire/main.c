7c06a7a52c968cd51e1943793c9d17e9cbc9bae5
Dustin Kröger <me@staubrein.com>
2019-11-08 13:49:36 +0100

    added timer

--- hotwire/hotwire/main.c
+++ hotwire/hotwire/main.c
@@ -13,9 +13,52 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-const int TASTER = PB6;
 volatile int running = 0;
 volatile int misses = 0;
+
+/************************************************************************/
+/* Timer                                                                */
+/************************************************************************/
+volatile int sec = 0;		// volatile => flüchtig, da in interrupt konfigurierbar sein muss
+volatile int count = 0;		// count Variable für 8-Bit-Timer
+
+void initTimer() {
+
+	TCCR0A |= (1 << TCW0);				// TCW0 Bit für Overflow Timer setzen -> sitzt in TCCR0A
+	TCCR0B |= (1 << CS01)|(1 << CS00);	// Prescaler auf 64 setzen (im Datenblatt schauen)
+
+	// Lowbyte zuerst lesen! Beachten: Zahlen sind immer die selben.
+	TCNT0L = (65535 - 15625) % 256;		// Zählregister Startwert vorstellen
+	TCNT0H = (65535 - 15625) / 256;		// << 8 (8x schieben entspricht geteilt durch 256)
+
+	TIMSK |= (1 << TOIE0);				// Timer Overflow interrupt aktivieren
+	sei();								// globales Aktivieren der Interrupts
+}
+
+ISR (TIMER0_OVF_vect) {
+
+	sec++;
+	if (sec == 60) {
+		sec = 0;
+	}
+	if(sec > 10) {
+		PORTA ^= 0b11111000;			// Nach dem Spiel blinken die LEDs
+		running = 0;
+	}
+	if(sec == 2)
+		PORTA = 0b10000000;
+	else if(sec % 2 == 0)
+		PORTA = (PORTA >> 1) | (1 << PA7) ;
+	
+	TCNT0L = (65535 - 15625) % 256;		// Zählregister Startwert wieder vorstellen
+	TCNT0H = (65535 - 15625) / 256;
+}
+
+/************************************************************************/
+/* Button Interrupt                                                     */
+/************************************************************************/
+
+const int TASTER = PB6;
 
 static void initInterrupt() {
 	
@@ -33,23 +76,34 @@ ISR(INT0_vect) {
 	
 		if(running == 0) {	// Einschalten.
 			
+			int current_sec = sec;
+			
+			while((PINB & (1 << TASTER)) == 0)
+			{
+				
+			}
 			running = 1;
+			//PORTB = 0b00001000;
+			//PORTA = 0b00000000;
 			PORTA = 0b11111000;
+			//draw7segment(0);
 			_delay_ms(500);
+			sec = 0;
 		}
 		else {				// Spiel läuft
 			PORTA=0;
 			_delay_ms(1000);	// 1s vorläufig bei Berührung
-			PORTA = 0b11111000;
+			PORTA = 0b11111011;
 		}
 	}
 	sei();
 }
 
-void draw7segment(void) {
+void draw7segment(int misses) {
 	
+	PORTA = 0;
 	switch(misses) {
-		case 0: 
+		case 0: // a(PA1) b(PA3) c(PA4) d(PA5) e(PA6) f(PA7)
 			break;
 		case 1: 
 			break;
@@ -69,19 +123,17 @@ void draw7segment(void) {
 			break;
 		case 9: 
 			break;
-		default: 
-			misses = 0;
-			break;
 	}
 }
 
 int main(void) {
 	
+	initTimer();
 	initInterrupt();
 	
 	DDRB |= (0 << TASTER); 	// Input for wire and button
 
-	DDRA |= 0b11111000;		// set side-A ports to output
+	DDRA |= 0b11111011;		// set side-A ports to output
 	DDRB |= (1 << PB3); 	// set pin4 of port b to output (LED/7Segment)
 
 	PORTA=0;				// set all pins of port a to low
